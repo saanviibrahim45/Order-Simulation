560a0ee9d90a4d6fd5efd1759f843d4625d89399
saanviibrahimpatnam <saanviibrahim45@gmail.com>
2025-06-20 00:26:18 -0400

    Updated by making a correct react project, the css and code placement is all messed up, need to update.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,25 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
 //add the rendering of the app here 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app'; // import your main component
+import App from './App'; // import your main component
 import './styles.css';   // optional styling
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
